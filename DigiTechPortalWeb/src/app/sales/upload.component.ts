3aca3da037d9fd3ed0eb1187385ab346e42d7e85
Kathiravan M <kathiravanmanjunathan2003@gmail.com>
2023-03-25 13:35:01 +0000

    Sales Code

--- DigiTechPortalWeb/src/app/sales/upload.component.ts
+++ DigiTechPortalWeb/src/app/sales/upload.component.ts
@@ -3,28 +3,30 @@ import { first } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 
-import { AccountService } from '@app/_services';
+import { SalesService } from '@app/_services';
 
 @Component({ templateUrl: 'upload.component.html' })
 export class UploadComponent implements OnInit {
 
-    constructor(private accountService: AccountService) { }
+    loading = false;
+    formData: FormData;
+
+    constructor(private salesService: SalesService) {
+        this.formData = new FormData();
+     }
 
     ngOnInit() {
 
     }
 
     onFileSelected(event: any) {
-        const file: File = event.target.files[0];
-        const formData = new FormData();
-        formData.append('file', file, file.name);
-        this.uploadFile(formData);
+        const file: File = event.target.files[0];        
+        this.formData.append('file', file, file.name);        
     }
 
-    uploadFile(formData: FormData) {
-        this.http.post('http://localhost:5000/upload', formData).subscribe(
-            (response) => console.log(response),
-            (error) => console.log(error)
-        );
+    onSubmit(){
+        this.loading = true;
+        this.salesService.uploadFile(this.formData);
+        this.loading = false;
     }
 }
